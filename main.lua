d8186588c6e3db1ba7ac1c501ac7c79520f41d84
HuyTheKiller <quochuynguyen0901@gmail.com>
2026-04-07 11:38:50 +0700

    Specify `icon_path`

--- main.lua
+++ main.lua
@@ -3,12 +3,14 @@ SilkTouch = SMODS.merge_defaults(SMODS.current_mod, SilkTouch)
 SMODS.load_file("src/drag_target.lua")()
 SMODS.load_file("src/controller_button.lua")()
 
-SMODS.Atlas{
-    key = "modicon",
-    path = "icon.png",
-    px = 34,
-    py = 34
-}
+if not SMODS.Atlases.stouch_modicon then
+    SMODS.Atlas{
+        key = "modicon",
+        path = "icon.png",
+        px = 34,
+        py = 34
+    }
+end
 
 SilkTouch.description_loc_vars = function()
     return { background_colour = G.C.CLEAR, text_colour = G.C.WHITE, scale = 1.2 }
